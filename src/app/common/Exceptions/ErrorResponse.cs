4f3ea6d7119c9a9a8d017ce74976f539b3f15687
kutegz <wilson.kutegeka@clinicmaster.net>
2024-06-21 17:42:14 +0300

    edited all namespaces to be file path from root.

--- src/app/common/Exceptions/ErrorResponse.cs
+++ src/app/common/Exceptions/ErrorResponse.cs
@@ -7,4 +7,11 @@ public sealed record ErrorResponse
     public required string Title { get; init; }
     public required string ErrorMessage { get; init; }
     public required string TraceId { get; init; }
+    public static ErrorResponse Empty => new() 
+    {
+        StatusCode = 0,
+        Title = string.Empty,
+        ErrorMessage = string.Empty,
+        TraceId = string.Empty
+    };
 }
