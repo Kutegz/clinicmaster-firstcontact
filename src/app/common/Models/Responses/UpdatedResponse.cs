7c518f97b76ad6162944f03c01f194cd6a8128f1
kutegz <wilson.kutegeka@clinicmaster.net>
2024-12-14 08:31:43 +0300

    Introduced HttpClientBase and UpdatedResponse

--- src/app/common/Models/Responses/UpdatedResponse.cs
+++ src/app/common/Models/Responses/UpdatedResponse.cs
@@ -1,11 +1,11 @@
 
 namespace ClinicMasterFirstContact.src.App.Common.Models.Responses;
-public sealed record CreatedResponse
+public sealed record UpdatedResponse
 {
     public required bool Success {get; init;}
     public required int StatusCode { get; init; }
     public required int Count {get; init;}
     public required string Message {get; init;}
     public required string Location {get; init;}
-    public required DateTime CreatedAt {get; init;}
+    public required DateTime UpdatedAt {get; init;}
 }
